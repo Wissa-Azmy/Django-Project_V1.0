53491534fbff196223f1c8f8ecdd69bf245b1a13
Wissa-Azmy <wisa.lfg@gmail.com>
2016-04-11 02:34:00 +0200

    version 1.1
    Facebook login Form added as the default one

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -20,6 +20,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    # url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^', include('blogApp.urls', namespace = 'articles')),
     url(r'^accounts/', include('allauth.urls')),    
 ]
